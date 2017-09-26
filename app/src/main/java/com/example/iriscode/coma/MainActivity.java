3d600e785e39fb98dc99d40ec61f1103675a3c32
unknown <hizassr@gmail.com>
2017-09-26 20:27:59 +0700

    fragment

--- app/src/main/java/com/example/iriscode/coma/MainActivity.java
+++ app/src/main/java/com/example/iriscode/coma/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.iriscode.coma;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HomeFragment fragmentHome = new HomeFragment();
+        ProfileFragment fragmentProfile = new ProfileFragment();
+        SettingFragment fragmentSetting = new SettingFragment();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(fragmentHome, "Home");
+        fragmentTransaction.add(fragmentProfile, "Profile");
+        fragmentTransaction.add(fragmentHome, "Home");
     }
 }
